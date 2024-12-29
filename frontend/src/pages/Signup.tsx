ff3b8f8f2842e78c54e70a6c54d8adb49bd51267
dhakarRaghu <rdhakad2002@gmail.com>
2024-12-29 19:03:22 +0530

    creating login page

---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,13 @@
+import { Auth } from "../components/Auth"
+import { Quote } from "../components/Quote"
+
+export const Signup = () => {
+    return <div>
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+            <div>
+                <Auth type="signup" />
+            </div>
+                <Quote />
+        </div>
+    </div>
+}
